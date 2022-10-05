e9a558f8b21883836ed97226ce5fcee0a1a050cd
Rabi3421 <rabinarayan3421@gmail.com>
2022-10-05 19:18:44 +0530

    navbar modified

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,53 @@
-import './Header.css'
+import {
+  faBed,
+  faCar,
+  faCartFlatbedSuitcase,
+  faFireBurner,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div className='header'>Header</div>
-  )
-}
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCartFlatbedSuitcase} />
+            <span>Flight + Hotel</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faFireBurner} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airpot Taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle#">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free booking account
+        </p>
+        <button className="headerBtn">Sign In / Register</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
