de042064909f4e42d1d7f129d0f6f374a77ac995
Rabi3421 <rabinarayan3421@gmail.com>
2022-10-06 16:54:50 +0530

    ui

---
+++ src/Components/PropertyList/PropertyList.jsx
@@ -0,0 +1,60 @@
+import "./PropertyList.css";
+
+const propertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          className="pListImg"
+          src="https://th.bing.com/th/id/OIP.1cc4aSnF9JP9Hf0_Ct1-AQHaDt?w=305&h=174&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          className="pListImg"
+          src="https://th.bing.com/th/id/OIP.rtsF36NM1Cm3dfH6ALMt-QHaFY?w=281&h=204&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        />
+        <div className="pListTitles">
+          <h1>Apartments</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          className="pListImg"
+          src="https://th.bing.com/th/id/OIP.I79tPE7ZUO3lQtytS8w6IgHaEe?w=273&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        />
+        <div className="pListTitles">
+          <h1>Resorts</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          className="pListImg"
+          src="https://th.bing.com/th/id/OIP.mmLDNaL9FEEefFbGmIDw_QHaE8?w=290&h=193&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        />
+        <div className="pListTitles">
+          <h1>Villas</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          className="pListImg"
+          src="https://th.bing.com/th/id/OIP.qY7z_bd3O60wFnKYFEHTIQHaEJ?w=324&h=182&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        />
+        <div className="pListTitles">
+          <h1>Cabins</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default propertyList;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Featured from '../../Components/Featured/Featured'
 import Header from '../../Components/Header/Header'
 import Navbar from '../../Components/Navbar/Navbar'
 import './Home.css'
+import PropertyList from '../../Components/PropertyList/PropertyList'
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
       <Header />
       <div className='homeContainer'>
         <Featured />
-        
+        <h1 className='homeTitle'>Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   )
