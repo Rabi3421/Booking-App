1fe38f4736116b5fd7c97eade787053883f1cac6
Rabi3421 <rabinarayan3421@gmail.com>
2022-10-07 17:13:34 +0530

    list done

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,39 +1,96 @@
-import './List.css'
-import Header from '../../Components/Header/Header'
-import Navbar from '../../Components/Navbar/Navbar'
-import { useLocation } from 'react-router-dom'
-import { useState } from 'react'
-
+import "./List.css";
+import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
   const location = useLocation();
   // console.log(location)
-  const [destination, setDestination] = useState(location.state.location)
-  const [date, setDate] = useState(location.state.date)
-  const [options, setOptions] = useState(location.state.options)
-
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
+  // console.log(destination)
   return (
     <div>
       <Navbar />
-      <Header type = "list"/>
-      <div className='listContainer'>
-        <div className='listWrapper'>
-          <div className='listSearch'>
-            <h1 className='lsTitle'>Search</h1>
-            <div className='lsItem'>
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
               <label>Destination</label>
-              <input type="text"/>
+              <input placeholder={destination} type="text" />
             </div>
-            <div className='lsItem'>
+            <div className="lsItem">
               <label>Check-In Date</label>
-              <span></span>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "dd/MM/yyyy"
+              )} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  ranges={date}
+                  minDate={new Date()}
+                />
+              )}
             </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Mix Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.adult}
+                    className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    placeholder={options.children}
+                    className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.room}
+                    className="lsOptionInput"
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
           </div>
-          <div className='listResult'></div>
+          <div className="listResult"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
