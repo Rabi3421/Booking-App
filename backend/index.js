abb1de4ee00ebc1ca5fab4e02fd87ba3b3ce25a2
Rabi3421 <rabinarayan3421@gmail.com>
2022-10-09 09:55:53 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 
 const app = express();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb");
+  } catch (err) {
+    throw err;
+  }
+};
 
-dotenv.config()
-
-app.listen(8080,()=>{
-    console.log("listenning on port 8080");
+mongoose.connection.on("connected",()=>{
+    console.log("MongoDb connected")
 })
+mongoose.connection.on("disconnected",()=>{
+    console.log("MongoDb disconnected")
+})
+
+app.listen(8080, () => {
+  connect();
+  console.log("listenning on port 8080");
+});
